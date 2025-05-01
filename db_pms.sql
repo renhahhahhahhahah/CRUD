2a875429f102a5702474ebc904ffc98fb52ce348
renhahhahhahhahah <154959721+renhahhahhahhahah@users.noreply.github.com>
2025-05-01 17:26:27 +0800

    Add files via upload

---
+++ db_pms.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2025 at 11:04 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_patient`
+--
+
+CREATE TABLE `tbl_patient` (
+  `patient_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_patient`
+--
+
+INSERT INTO `tbl_patient` (`patient_id`, `name`, `address`) VALUES
+(8, 'miyawaki sakura', 'Kagoshima City, Japan'),
+(10, 'kim chaewon', 'Seoul, South Korea.'),
+(11, 'mai sakurajima', ' Fujizawa, Kanagawa Prefecture, Japan'),
+(12, 'huh yunjin', ' New York, USA'),
+(13, 'Hong eunchae', 'Miryang, South Korea'),
+(14, 'Nakamura Kazuha', 'Osaka, Japan'),
+(15, 'Kim Garam', 'Sangju, Gyeongsangbuk-do, South Korea');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_patient`
+--
+ALTER TABLE `tbl_patient`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_patient`
+--
+ALTER TABLE `tbl_patient`
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
